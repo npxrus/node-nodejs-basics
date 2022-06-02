9b7ab6cd68059ad823d3557b6b80b2ce8ec939ad
npxrus <npxrus@yandex.com>
2022-06-02 21:40:11 +0300

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,23 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 export const rename = async () => {
-    // Write your code here 
+  const oldFileName = 'wrongFilename.txt';
+  const newFileName = 'properFilename.md';
+  const oldFilePath = join('./files', oldFileName);
+  const newFilePath = join('./files', newFileName);
+  const errorMessage = 'FS operation failed';
+
+  try {
+    await promises.stat(oldFilePath);
+    try {
+      await promises.stat(newFilePath);
+      console.error(errorMessage);
+    } catch (e) {
+      await promises.rename(oldFilePath, newFilePath);
+    }
+  } catch (e) {
+    console.error(errorMessage);
+  }
 };
+rename();
