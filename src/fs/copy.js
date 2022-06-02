0e8a56ed56abe816b3c8abd1c8e6f8a512a7a908
npxrus <npxrus@yandex.com>
2022-06-02 21:03:52 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,18 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 export const copy = async () => {
-    // Write your code here 
+  const srcDirName = './files';
+  const destDirName = './files_copy';
+  const errorMessage = 'FS operation failed';
+
+  try {
+    if (await promises.opendir(destDirName)) {
+      console.error(errorMessage);
+    }
+  } catch (e) {
+    await promises.cp(srcDirName, destDirName, { recursive: true });
+  }
 };
+
+copy();
