fa7660c76d16c6a2ed828cfd1da1d2f2272d12fa
npxrus <npxrus@yandex.com>
2022-06-02 20:27:32 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,18 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 export const create = async () => {
-    // Write your code here 
+  const fileName = 'fresh.txt';
+  const fileContent = 'I am fresh and young';
+  const filePath = join('./files', fileName);
+  const errorMessage = 'FS operation failed';
+
+  try {
+    await promises.stat(filePath);
+    console.error(errorMessage);
+  } catch (e) {
+    await promises.writeFile(filePath, fileContent);
+  }
 };
+
+create();
