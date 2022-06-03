7e77b1d38afb6c74f1f46da6742681976c1a6ca4
npxrus <npxrus@yandex.com>
2022-06-03 20:33:53 +0300

    feat: add parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,13 @@
+import { env } from 'process';
+
 export const parseEnv = () => {
-    // Write your code here 
+  const varNames = Object.keys(env).filter((key) => key.startsWith('RSS_'));
+
+  try {
+    varNames.forEach((name) => console.log(`${name}=${env[name]}`));
+  } catch (e) {
+    console.error(e);
+  }
 };
+
+parseEnv();
