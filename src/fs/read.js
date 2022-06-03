e25689a66d1de37021d82687f6e5282eef9996b6
npxrus <npxrus@yandex.com>
2022-06-03 19:52:27 +0300

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 export const read = async () => {
-    // Write your code here 
+  const fileName = 'fileToRead.txt';
+  const filePath = join('./files', fileName);
+  const errorMessage = 'FS operation failed';
+
+  try {
+    await promises.stat(filePath);
+    const fileContent = await promises.readFile(filePath);
+    console.log(fileContent.toString());
+  } catch (e) {
+    console.error(errorMessage);
+  }
 };
+
+read();
