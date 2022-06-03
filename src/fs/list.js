f3dae387cd3fca83f57a70e1fe0e5c47c6b17a60
npxrus <npxrus@yandex.com>
2022-06-03 19:31:54 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,17 @@
+import { promises } from 'fs';
+
 export const list = async () => {
-    // Write your code here 
+  const dirName = './files';
+  const errorMessage = 'FS operation failed';
+
+  try {
+    const files = await promises.readdir(dirName);
+    for (const file of files) {
+      console.log(file);
+    }
+  } catch (e) {
+    console.error(errorMessage);
+  }
 };
+
+list();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -20,4 +20,5 @@ export const rename = async () => {
     console.error(errorMessage);
   }
 };
+
 rename();
