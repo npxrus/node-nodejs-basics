2e1879b8c86bb2f7c1b40385a3621d2cf02339cb
npxrus <npxrus@yandex.com>
2022-06-03 18:50:00 +0300

    feat: add remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,17 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 export const remove = async () => {
-    // Write your code here 
+  const fileName = 'fileToRemove.txt';
+  const filePath = join('./files', fileName);
+  const errorMessage = 'FS operation failed';
+
+  try {
+    await promises.stat(filePath);
+    await promises.rm(filePath);
+  } catch (e) {
+    console.error(errorMessage);
+  }
 };
+
+remove();
