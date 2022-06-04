ada12239ba7efc9e3ffab5a93edd4c47a885e067
npxrus <npxrus@yandex.com>
2022-06-04 15:55:31 +0300

    feat: add decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,20 @@
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
 export const decompress = async () => {
-    // Write your code here 
+  const archiveName = './files/archive.gz';
+  const fileName = './fileToCompress.txt';
+
+  try {
+    await pipeline(
+      createReadStream(archiveName),
+      createGunzip(),
+      createWriteStream(fileName)
+    );
+  } catch (e) {
+    console.error(e);
+  }
 };
+
+decompress();
