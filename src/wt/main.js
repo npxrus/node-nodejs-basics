7c1e76d8cf04578005f0b194a0776468627c6f80
npxrus <npxrus@yandex.com>
2022-06-04 20:01:23 +0300

    feat: add performCalculations function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,30 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
 export const performCalculations = async () => {
-    // Write your code here
+  let workers = [];
+  for (let i = 0; i < cpus().length; i++) {
+    workers.push(new Worker('./worker.js', { workerData: i + 10 }));
+  }
+
+  const workersArray = workers.map((worker) => {
+    return new Promise((resolve, reject) => {
+      worker.on('message', (msg) => {
+        resolve({ status: 'resolved', data: msg });
+      });
+
+      worker.on('error', (err) => {
+        reject({ status: 'error', data: null });
+      });
+    });
+  });
+
+  try {
+    const result = await Promise.all(workersArray);
+    return result;
+  } catch (e) {
+    console.error(e.message);
+  }
 };
+
+performCalculations();
