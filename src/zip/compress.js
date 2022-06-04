6d33c068a342e9a40663fd6a8e5b9d562603d5d9
npxrus <npxrus@yandex.com>
2022-06-04 15:50:43 +0300

    feat: add compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,20 @@
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 export const compress = async () => {
-    // Write your code here 
+  const fileName = './files/fileToCompress.txt';
+  const archiveName = './files/archive.gz';
+
+  try {
+    await pipeline(
+      createReadStream(fileName),
+      createGzip(),
+      createWriteStream(archiveName)
+    );
+  } catch (e) {
+    console.error(e);
+  }
 };
+
+compress();
