675f52ec9a145483bf5c24df2e8905b1b40f03f0
npxrus <npxrus@yandex.com>
2022-06-04 15:26:56 +0300

    feat: add transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,14 @@
+import { pipeline, Transform } from 'stream';
+import { stdin, stdout } from 'process';
+
 export const transform = async () => {
-    // Write your code here 
+  const reverseData = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString().split('').reverse().join(''));
+    },
+  });
+
+  pipeline(stdin, reverseData, stdout, () => {});
 };
+
+transform();
