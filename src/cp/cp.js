17b1a601f7bd9661e332fb2931fbdc0a4bed7384
npxrus <npxrus@yandex.com>
2022-06-05 08:34:31 +0300

    feat: add spawnProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,9 @@
+import { fork } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const fileName = './files/script.js';
+
+  const forkProcess = fork(fileName, [...args]);
 };
+
+spawnChildProcess(['one', 'two', 'three']);
